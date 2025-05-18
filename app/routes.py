5cd437c0a5296a8beebc3daab7a4ff04ae91be38
VladislawChorny <cherny.for.work@gmail.com>
2025-05-18 16:28:06 +0300

    Add login in db

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,10 @@
 from flask import render_template, flash, redirect, url_for
-from app import app
+from app import app 
+from flask_login import current_user, login_user
+from flask_login import logout_user
+import sqlalchemy as sa
+from app import db
+from app.models import User
 from app.forms import LoginForm
 
 @app.route('/')
@@ -21,14 +26,22 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        #Вывод пользователю сообщения об успешности выполнения процедуры
-        flash('Login requested for user {}, remeber_me={}'.format(form.username.data, 
-                                                                  form.remember_me.data))
+        user = db.session.scalar(
+            sa.select(User).where(user.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remeber=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
 
-
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
